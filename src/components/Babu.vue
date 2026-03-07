b24fa7a45b613f571bd61274b22d861cf7f458d5
Melody <118788470+SongAndMelody@users.noreply.github.com>
2026-03-07 15:25:23 -0500

    changed image urls (public folder is served at root)

--- src/components/Babu.vue
+++ src/components/Babu.vue
@@ -14,7 +14,7 @@
 	border-image-width: 10px 10px 10px 10px;
 	border-image-outset: 0px 0px 0px 0px;
 	border-image-repeat: round round;
-	border-image-source: url("/public/images/buttons/button_blue.png");
+	border-image-source: url("/images/buttons/button_blue.png");
 	border-style: solid;
 	image-rendering: crisp-edges;
 	font-family: "Baba UI", sans-serif;
@@ -26,14 +26,14 @@
 }
 
 .babu:hover {
-	border-image-source: url("/public/images/buttons/button_blue_hover.png");
+	border-image-source: url("/images/buttons/button_blue_hover.png");
 }
 
 .babu:active {
-	border-image-source: url("/public/images/buttons/button_blue_press.png");
+	border-image-source: url("/images/buttons/button_blue_press.png");
 }
 
 .babu:disabled {
-	border-image-source: url("/public/images/buttons/button_disabled.png");
+	border-image-source: url("/images/buttons/button_disabled.png");
 }
 </style>
