5e16b2ae413a4377065a82cd4c447efac0f1431d
luco <89469304+luwucoso@users.noreply.github.com>
2026-03-07 21:02:12 -0300

    fixed card sizes

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -9,41 +9,38 @@ const route = useRoute()
 const query = route.query.q
 
 watch(
-    () => route.query.q,
-    (newQuery) => {
-        // i do not know how vue router works
-        // so im just gonna reload the page
-        // bad bad idea i know
-        globalThis.location.reload()
-    },
+        () => route.query.q,
+        (newQuery) => {
+                // i do not know how vue router works
+                // so im just gonna reload the page
+                // bad bad idea i know
+                globalThis.location.reload()
+        },
 )
 
 const results = ref<SearchResult[]>(lies);
 </script>
 
 <template>
-    <h1>Search results for {{ query }}</h1>
-    <div class="grid">
-        <div v-for="({ name, author, desc }, idx) in results">
-            <SearchPack :name :author :desc />
-            <!--If we render more than 3 cards in a line, go to the next one-->
-            <!--TODO: we should replace this with a dynamic check based on the card's length-->
-            <!--We also need to fix card heights so they're all consistent (ideally matching the tallest one of having a set height)-->
-            <div v-if="idx % 3 >= 2">
-
-            </div>
+        <h1>Search results for {{ query }}</h1>
+        <div class="grid">
+                <div v-for="({ name, author, desc }, idx) in results">
+                        <!--TODO: we should replace this with a dynamic check based on the card's length-->
+                        <!--We also need to fix card heights so they're all consistent (ideally matching the tallest one of having a set height)-->
+                        <SearchPack :name :author :desc />
+                </div>
         </div>
-    </div>
 </template>
 
 <style scoped>
 h1,
 p {
-    color: white
+        color: white
 }
 
 .grid {
-    display: grid;
-    grid-template-columns: auto auto auto;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        gap: 1rem;
 }
 </style>
