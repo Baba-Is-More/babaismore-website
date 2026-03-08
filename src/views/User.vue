7d84044d5afc53ad8b22b5b49d372b4b396e1542
luco <89469304+luwucoso@users.noreply.github.com>
2026-03-08 18:57:29 -0300

    huuh?

---
+++ src/views/User.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Babu from '@/components/Babu.vue'
+
+const props = defineProps<{
+	name: string
+}>()
+
+</script>
+
+<template>
+	<h1>User uhhhhhhh</h1>
+</template>
+
+<style scoped>
+h1,
+p {
+	color: white
+}
+</style>
