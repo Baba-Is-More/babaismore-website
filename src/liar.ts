7854088ea2995890558f3f49452a6b0b402faebc
Melody <118788470+SongAndMelody@users.noreply.github.com>
2026-03-08 17:18:02 -0400

    Cards now display when they were posted (in "x ys ago")

--- src/liar.ts
+++ src/liar.ts
@@ -14,28 +14,28 @@ export default [
 		author: "Plasmaflare",
 		desc: "A simple mod for adding baserules to your levelpack.",
 		downloads: 140,
-		posted: new Date()
+		posted: new Date(2024, 10, 3, 1, 30)
 	},
 	{
 		name: "Menu Background Randomizer",
 		author: "Extrem",
 		desc: "Randomizes the background of the titlescreen.",
 		downloads: 417,
-		posted: new Date()
+		posted: new Date(2024, 2, 10, 2, 30)
 	},
 	{
 		name: "Proverb Mod",
 		author: "Pigeon",
 		desc: "Allows an easy way to create and manage custom verbs.",
 		downloads: 0,
-		posted: new Date()
+		posted: new Date("2025-04-17")
 	},
 	{
 		name: "(untitled mod)",
 		author: "DizzyAndy",
 		desc: "A large selection of custom words, building off of the vanilla ones.",
 		downloads: 1.46e7,
-		posted: new Date()
+		posted: new Date("2011-10-10T14:48:00.000+09:00")
 	},
 	{
 		name: "Blank Mod",
@@ -49,6 +49,6 @@ export default [
 		author: "Extrem",
 		desc: "Upgrades the levelpack navigation menus by providing a lot of utilities, such as levelpack pinning and showing level counts.",
 		downloads: 35050,
-		posted: new Date()
+		posted: new Date(99, 1, 2, 3, 4)
 	},
 ] as SearchResult[];
