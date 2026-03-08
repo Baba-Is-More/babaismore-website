7895606cfeb3ca73411a11c2f8b1d6996441bfbb
Melody <118788470+SongAndMelody@users.noreply.github.com>
2026-03-07 20:11:59 -0500

    project descriptions now truncate (current limit is set to 128 characters, this can be changed in SearchPack)

--- src/components/SearchPack.vue
+++ src/components/SearchPack.vue
@@ -2,6 +2,15 @@
 import type { SearchResult } from '@/liar.ts';
 
 const props = defineProps<SearchResult>()
+
+function truncate(desc: string): string {
+	const max_length: number = 128
+	if (desc.length >= 128) {
+		return desc.substring(0, max_length - 3).concat("...");
+	} else {
+		return desc;
+	}
+}
 </script>
 
 <template>
@@ -9,7 +18,7 @@ const props = defineProps<SearchResult>()
 		<!--todo: unroughten this-->
 		<h2> {{ name }} </h2>
 		<h3> {{ author }} </h3>
-		<p> {{ desc }} </p>
+		<p> {{ truncate(desc) }} </p>
 	</div>
 </template>
 
