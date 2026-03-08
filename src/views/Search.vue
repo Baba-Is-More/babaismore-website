ea25e2260c4351a15aaba9274ed774796ce51e60
Melody <118788470+SongAndMelody@users.noreply.github.com>
2026-03-07 19:36:20 -0500

    formatting

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -9,38 +9,38 @@ const route = useRoute()
 const query = route.query.q
 
 watch(
-        () => route.query.q,
-        (newQuery) => {
-                // i do not know how vue router works
-                // so im just gonna reload the page
-                // bad bad idea i know
-                globalThis.location.reload()
-        },
+    () => route.query.q,
+    (newQuery) => {
+        // i do not know how vue router works
+        // so im just gonna reload the page
+        // bad bad idea i know
+        globalThis.location.reload()
+    },
 )
 
 const results = ref<SearchResult[]>(lies);
 </script>
 
 <template>
-        <h1>Search results for {{ query }}</h1>
-        <div class="grid">
-                <div v-for="({ name, author, desc }, idx) in results">
-                        <!--TODO: we should replace this with a dynamic check based on the card's length-->
-                        <!--We also need to fix card heights so they're all consistent (ideally matching the tallest one of having a set height)-->
-                        <SearchPack :name :author :desc />
-                </div>
+    <h1>Search results for {{ query }}</h1>
+    <div class="grid">
+        <div v-for="({ name, author, desc }, idx) in results">
+            <!--TODO: we should replace this with a dynamic check based on the card's length-->
+            <!--We also need to fix card heights so they're all consistent (ideally matching the tallest one of having a set height)-->
+            <SearchPack :name :author :desc />
         </div>
+    </div>
 </template>
 
 <style scoped>
 h1,
 p {
-        color: white
+    color: white
 }
 
 .grid {
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        gap: 1rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 1rem;
 }
 </style>
