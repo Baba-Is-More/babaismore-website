757f4278c33c1534719674a547acb566ac69c4d8
Melody <118788470+SongAndMelody@users.noreply.github.com>
2026-03-07 20:58:36 -0500

    Added section for profile picture displaying

--- src/components/SearchPack.vue
+++ src/components/SearchPack.vue
@@ -4,7 +4,7 @@ import type { SearchResult } from '@/liar.ts';
 const props = defineProps<SearchResult>()
 
 function truncate(desc: string): string {
-	const max_length: number = 128
+	const max_length: number = 256
 	if (desc.length >= 128) {
 		return desc.substring(0, max_length - 3).concat("...");
 	} else {
@@ -15,9 +15,8 @@ function truncate(desc: string): string {
 
 <template>
 	<div class="base">
-		<!--todo: unroughten this-->
 		<h2> {{ name }} </h2>
-		<h3> {{ author }} </h3>
+		<h3> <img src="\images\image_baba.png" alt="{{ author }}"> {{ author }} </h3>
 		<p> {{ truncate(desc) }} </p>
 	</div>
 </template>
@@ -29,9 +28,20 @@ function truncate(desc: string): string {
 	align-items: center;
 	margin: 5px;
 	height: 100%;
+	padding: 0px 2px
+}
+
+h3 {
+	font-size: 10px;
+}
+
+img {
+	width: 25px;
+	height: 25px;
+	vertical-align: bottom;
 }
 
 p {
-	font-size: small;
+	font-size: x-small;
 }
 </style>
