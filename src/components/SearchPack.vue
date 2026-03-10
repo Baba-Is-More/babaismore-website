92a2c195830857a724ee719909d5e156ed6080a9
Siwakornpc <proarm2009@gmail.com>
2026-03-10 16:47:24 +0700

    Changed text size to 8px (smallest and best for reading), Changed search packs visuals.

--- src/components/SearchPack.vue
+++ src/components/SearchPack.vue
@@ -31,7 +31,7 @@ function dateToOffset(date: Date): string {
 
 	let now: Date = new Date();
 	let elapsed: number = now.valueOf() - date.valueOf();
-	const rtf = new Intl.RelativeTimeFormat();
+	const rtf = new Intl.RelativeTimeFormat("en", { numeric: "auto" });;
 
 	if (elapsed < msPerMinute) {
 		return rtf.format(-Math.floor(elapsed / 1000), "seconds");
@@ -52,15 +52,19 @@ function dateToOffset(date: Date): string {
 
 <template>
 	<div class="base blue" id="box">
-		<div>
-			<img src="\images\image_levelpacks.png" alt="{{ name }}" id="icon">
-			<p class="name"> {{ name }} </p>
-			<p class="author"> <img src="\images\image_baba.png" alt="{{ author }}" class="inline"> {{
-				author }} </p>
-			<p class="info"> <img src="\images\image_downloads.png" alt="{{ downloads }} downloads" class="inline"> {{
-				reduce(downloads) }} <img src="\images\image_clock.png" alt="" class="inline"> {{
-					dateToOffset(posted)
-				}}</p>
+		<div class="horizontal-align">
+			<div>
+				<img src="\images\image_levelpacks.png" alt="{{ name }}" id="icon">
+			</div>
+			<div>
+				<p class="name"> {{ name }} </p>
+				<p class="author"> <img src="\images\image_baba.png" alt="{{ author }}" class="inline"> {{
+					author }} </p>
+				<p class="info"> <img src="\images\image_downloads.png" alt="{{ downloads }} downloads" class="inline"> {{
+					reduce(downloads) }} <img src="\images\image_clock.png" alt="" class="inline"> {{
+						dateToOffset(posted)
+					}}</p>
+			</div>
 		</div>
 		<p class="desc"> {{ truncate(desc) }} </p>
 		<div>
@@ -77,7 +81,7 @@ function dateToOffset(date: Date): string {
 	align-items: center;
 	height: 100%;
 	border-image-slice: 8 8 8 8 fill;
-	border-image-width: 10px 10px 10px 10px;
+	border-image-width: 8px 8px 8px 8px;
 	border-image-outset: 0px 0px 0px 0px;
 	border-image-repeat: round round;
 	border-style: solid;
@@ -86,8 +90,8 @@ function dateToOffset(date: Date): string {
 }
 
 #box {
-	display: flexbox;
-	padding: 0px 10px;
+    box-sizing: border-box;
+    padding: 4px 10px 74px 10px;
 }
 
 .blue {
@@ -107,8 +111,8 @@ function dateToOffset(date: Date): string {
 }
 
 .inline {
-	width: 31px;
-	height: 31px;
+	width: 24px;
+	height: 24px;
 	vertical-align: top;
 }
 
@@ -126,23 +130,24 @@ function dateToOffset(date: Date): string {
 	margin-bottom: 5px;
 	float: inline-start;
 	padding: 2px 7px 2px 7px;
-	font-size: 6px;
+	font-size: 8px;
 }
 
 p {
-	font-size: 12px;
 	margin: 0px;
 }
 
 .name {
 	margin: 4px;
+	text-align: left;
 }
 
 #icon {
-	width: 128px;
-	height: 128px;
 	vertical-align: top;
-	float: left;
 	margin: 1px;
+}
+
+.horizontal-align {
+	display: flex;
 }
 </style>
