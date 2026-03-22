9789487e92769b472bfbfa2e2f744e1a901ab43b
luco <89469304+luwucoso@users.noreply.github.com>
2026-03-22 01:24:52 -0300

    working on docker integration

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,21 +7,14 @@ import * as trpcExpress from "@trpc/server/adapters/express";
 import express from "express";
 import path from "node:path";
 import { createContext } from "./context";
-import bcrypt from "bcrypt";
 import cookieParser from "cookie-parser";
 
-let temp_has_mongodb = false;
-
 if (process.env.DB_URL) {
-    // note: you need to add your own `.env` file to the root of the project with DB_URL
     await mongoose.connect(process.env.DB_URL);
-    temp_has_mongodb = true;
 } else {
     throw "no db found on .env! please create a key 'DB_URL' with 'mongodb+srv://' url";
-    // implied temp_has_mongodb = false;
 }
 
-export const has_mongodb = temp_has_mongodb;
 export const is_dev = true;
 
 const app = express();
@@ -37,7 +30,7 @@ app.use(
 
 app.use(
     "/uploads",
-    express.static(path.join(import.meta.dirname, "../../", "uploads")),
+    express.static(path.join(import.meta.dirname, "../../", "app/", "uploads")),
 );
 
 console.log("listening on http://localhost:3000...");
