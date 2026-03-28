952f32d891a95f291c64343fb752cda1003ff395
luco <89469304+luwucoso@users.noreply.github.com>
2026-03-28 16:52:07 -0300

    ACTUAL footer fix

--- src/client/views/Login.vue
+++ src/client/views/Login.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-    <p>login</p>
+    <div class="log-sign-in">
+        <h1>hi</h1>
+    </div>
 </template>
 
 <style>
 * {
     color: white;
+}
+
+.log-sign-in {
+    background-color: #293141;
+    display: flex;
+    margin: 100px;
+    border-radius: 5px;
 }
 </style>
